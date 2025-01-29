ea57ea174522da112b8b8f799096df3217b44d3d
tzqgav10 <e1155947@u.nus.edu>
2025-01-29 17:28:15 +0800

    Add scanner to take in user input
    User can give input to Chatbot and it will respond according to the
    input. It can either add task, print the list of tasks or exit when
    "bye" is entered as input.

--- src/main/java/Peira.java
+++ src/main/java/Peira.java
@@ -1,10 +1,31 @@
+import java.util.Scanner;
+
 public class Peira {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Peira.");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Hello! I'm Peira.");
+        System.out.println("    What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+
+        while (true) {
+            String line;
+            Scanner in = new Scanner(System.in);
+            line = in.nextLine();
+            if (line.contains("bye")) {
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    Bye. Hope to see you again soon!");
+                System.out.println("    ____________________________________________________________");
+                break;
+            } else if (line.contains("list")){
+                System.out.println("    ____________________________________________________________");
+                Task.printTasks();
+                System.out.println("    ____________________________________________________________");
+            } else {
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    added: " + line);
+                Task.addTask(line);
+                System.out.println("    ____________________________________________________________");
+            }
+        }
     }
 }
